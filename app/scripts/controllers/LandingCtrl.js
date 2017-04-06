3ac30fd7c3f5123cee338ba2c310cc9f8009cfc7
jamisonrubino <jamison.rubino@gmail.com>
2017-04-06 16:37:40 -0700

    Checkpoint 4 in progress. Rewired controllers.

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,28 +1,6 @@
 (function() {
 	function LandingCtrl(Room) {
 		this.Room = Room;
-		
-		// MODAL CODE
-		this.modalValue = null;
-		this.openModal = function () {
-			NewRoom.modalInstance = $uibModal.open({
-				size: sm,
-				animation: true,
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				templateUrl: 'newroommodal.html'
-			});
-		};
-		this.close = function () {
-			this.modalInstance.dismiss();
-		};
-		this.submit = function(value){
-			this.modalValue = $("#modal-value").val();
-			console.log(this.modalValue);
-			if (this.modalValue.length > 0) {
-				Room.newRoom(this.modalValue);
-			}		
-		};
 
 // Temporary code / replaced by modal
 //		var myRoom = "room5";
--- app/scripts/controllers/NewRoomModalCtrl.js
+++ app/scripts/controllers/NewRoomModalCtrl.js
@@ -1,6 +1,27 @@
 (function() {
 	function NewRoomModalCtrl(Room) {
 
+		// MODAL CODE
+		this.modalValue = null;
+		this.openModal = function () {
+			NewRoom.modalInstance = $uibModal.open({
+				size: sm,
+				animation: true,
+				ariaLabelledBy: 'modal-title',
+				ariaDescribedBy: 'modal-body',
+				templateUrl: 'newroommodal.html'
+			});
+		};
+		this.close = function () {
+			this.modalInstance.dismiss();
+		};
+		this.submit = function(value){
+			this.modalValue = $("#modal-value").val();
+			console.log(this.modalValue);
+			if (this.modalValue.length > 0) {
+				Room.newRoom(this.modalValue);
+			}		
+		};
 	}
 	
 	angular
